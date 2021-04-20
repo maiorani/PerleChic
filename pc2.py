addacf17925d06eefdd590fa6e9c9002ea3f1ffa
maiorani <69973235+maiorani@users.noreply.github.com>
2021-04-20 16:04:42 -0300

    Version 2

---
+++ pc2.py
@@ -0,0 +1,28 @@
+preco_cm = 0.0025
+mdo1 = 3.70
+mdo2 = 4.10
+
+
+def pulseira(cm_pulso, cm_disco):
+    a = preco_cm * cm_pulso
+    b = (cm_disco + 1) * 0.18
+    pf = a + b + mdo1
+    return '%.2f' % pf
+
+
+def colar(cm_pescoco, cm_disco):
+    a = preco_cm * cm_pescoco
+    b = (cm_disco) * 0.18
+    pf = a + b + mdo2
+    return '%.2f' % pf
+
+
+opcao = input('Se trata de uma pulseira ou de um colar? ')
+if opcao == 'pulseira':
+    cm_pulso = float(input('Insira os centímetros da pulseira: '))
+    cm_disco = float(input('Insira os centímetros do disco: '))
+    print(pulseira(cm_pulso, cm_disco))
+elif opcao == 'colar':
+    cm_pescoco = float(input('Insira os centímetros do pescoço: '))
+    cm_disco = float(input('Insira os centímetros do disco: '))
+    print(colar(cm_pescoco, cm_disco))
