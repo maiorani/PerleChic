87eadff829446a69cb331890740f2578efdc8c47
maiorani <69973235+maiorani@users.noreply.github.com>
2021-04-20 16:05:14 -0300

    Version 3

---
+++ pc3.py
@@ -0,0 +1,38 @@
+preco_cm1 = 0.0025
+preco_cm2 = 0.00068
+mdo1 = 3.70
+mdo2 = 4.10
+
+
+def pulseira(cm_pulso, cm_disco):
+    a = preco_cm1 * cm_pulso
+    b = (cm_disco + 1) * 0.18
+    pf = a + b + mdo1
+    return '%.2f' % pf
+
+
+def colar(cm_pescoco, cm_disco):
+    a = preco_cm2 * cm_pescoco
+    b = (cm_disco) * 0.18
+    pf = a + b + mdo2
+    return '%.2f' % pf
+
+
+while True:
+    opcao = input('Digite 1 para pulseira / Digite 2 para colar: ')
+    if opcao == '1':
+        cm_pulso = float(input('Insira os centímetros da pulseira: '))
+        cm_disco = float(input('Insira os centímetros do disco: '))
+        print('O custo de produção é: R$', pulseira(cm_pulso, cm_disco))
+        print('O custo de venda é: R$', float(pulseira(cm_pulso, cm_disco)) * 3)
+        break
+    elif opcao == '2':
+        cm_pescoco = float(input('Insira os centímetros do pescoço: '))
+        cm_disco = float(input('Insira os centímetros do disco: '))
+        print('O custo de produção é: R$', (colar(cm_pescoco, cm_disco)))
+        print('O custo de venda é: R$', float(colar(cm_pescoco, cm_disco)) * 3)
+        break
+    else:
+        print('Valor inexistente.')
+
+print('Obrigado por utilizar nosso programa')
